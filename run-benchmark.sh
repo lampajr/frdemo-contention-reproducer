119af356210460435940f322834247e0495567d3
Andrea Lamparelli <a.lamparelli95@gmail.com>
2025-09-04 08:57:32 +0200

    Fix hyperfoil report name
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -6,6 +6,8 @@ CWD="$(dirname "$0")"
 ASPROF_PARAMS=${ASPROF_PARAMS:-"--jfrsync profile -e cpu,lock"}
 ASPROF_OUT_FILENAME=${ASPROF_OUT_FILENAME:-"standalone"}
 
+HF_REPORT=${HF_REPORT:-"/tmp/report_${ASPROF_OUT_FILENAME}.html"}
+
 # retrieve the pid
 export STANDALONE_PID=$(jps | grep jboss-modules.jar | awk '{ print $1}')
 
@@ -13,6 +15,6 @@ echo "Attaching the profiler"
 
 bash ${CWD}/start-async-profiler.sh
 
-jbang run@hyperfoil -o /tmp/report.html ${CWD}/benchmark.hf.yaml
+jbang run@hyperfoil -o ${HF_REPORT} ${CWD}/benchmark.hf.yaml
 
 bash ${CWD}/stop-async-profiler.sh
